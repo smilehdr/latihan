80fd4e77ace97b3eff3493f3f03cf53712e2ccb2
smilehdr <smilehdr@gmail.com>
2018-05-30 15:16:02 +0700

    loopingChallengeRev

--- loopingChallenge.js
+++ loopingChallenge.js
@@ -40,6 +40,23 @@ for(var i=1;i<=100; i++) {
     }
 }
 
+for(var i =1;i<=100;i= i+2) {
+    if(i % 3 == 0) {
+        console.log( i + " kelipatan 3");
+    }
+}
+for(var i =1;i<=100;i= i+5) {
+    if(i % 6 == 0) {
+        console.log( i + " kelipatan 6");
+    }
+}
+
+for(var i =1;i<=100;i= i+9) {
+    if(i % 10 == 0) {
+        console.log( i + " kelipatan 10");
+    }
+}
+
 //Menyusun Barisan Bintang
 var rows1 = 5;
 for(var i = 1; i<=rows1;i++) {
