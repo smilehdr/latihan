9ab4bf4147502edf17e46d0c2066f19fc2107587
smilehdr <smilehdr@gmail.com>
2018-05-30 14:40:24 +0700

    loopingChallenge

---
+++ loopingChallenge.js
@@ -0,0 +1,67 @@
+var flag = 1;
+console.log("LOOPING PERTAMA");
+while(flag<=20) {
+    if(flag % 2 == 0) {
+        console.log(flag + "  - I love coding");
+    }
+    flag++;
+}
+
+console.log("LOOPING KEDUA");
+while(flag>=2) {
+    if(flag % 2 == 0) {
+        console.log(flag + "  - I will become  developer");
+    }
+    flag--;
+}
+
+//Melakukan Looping Menggunakan For
+console.log("LOOPING PERTAMA");
+for(var i = 1;i<=20; i++) {
+    if(i % 2 == 0) {
+        console.log(i + "  - I love coding");
+    }
+}
+
+console.log("LOOPING KEDUA");
+for( i = 20;i>=2; i--) {
+    if(i % 2 == 0) {
+        console.log(i + "  - I will become  developer");
+    }
+}
+
+//Angka Ganjil dan Genap
+for(var i=1;i<=100; i++) {
+    if(i%2 == 0) {
+        console.log("GENAP ");
+    }
+    else {
+        console.log("GANJIL ");
+    }
+}
+
+//Menyusun Barisan Bintang
+var rows1 = 5;
+for(var i = 1; i<=rows1;i++) {
+    console.log("*");
+}
+
+var rows2 = 5;
+var output2 = "";
+for(var i = 1;i<=rows2; i++) {
+    output2 = "";
+    for(var x=1;x<=rows2;x++) {
+        output2 += "*";
+    }
+    console.log(output2);
+}
+
+var rows3 = 5;
+var output3 = "";
+for(var i = 1;i<=rows3; i++) {
+    output3 = "";
+    for(var x=1;x<=i;x++) {
+        output3 += "*";
+    }
+    console.log(output3);
+}
